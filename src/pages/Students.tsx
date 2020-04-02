82b9d39134db97eee4326ee049151b5db941ff08
Michael Callaghan <mikecallaghan67@gmail.com>
2020-04-01 22:25:07 -0400

    Got most of students page done

--- src/pages/Students.tsx
+++ src/pages/Students.tsx
@@ -1,7 +1,68 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonMenuButton, IonList, IonItemSliding, IonItem, IonIcon, IonLabel, IonItemOptions, IonItemOption } from '@ionic/react';
-import React from 'react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonMenuButton, IonList, IonItemSliding, IonItem, IonIcon, IonLabel, IonItemOptions, IonItemOption, IonActionSheet } from '@ionic/react';
+import { eyeOffOutline, eyeOutline, personOutline } from 'ionicons/icons';
+import React, { useState } from 'react';
+
+enum Presence {
+  Unknown,
+  Absent,
+  Present
+}
+interface Student {
+  id: string;
+  firstName: string;
+  lastName: string;
+  birthDate?: Date;
+  parentName?: string;
+  parentEmail?: string;
+  parentPhone?: string;
+  photoUrl?: string;
+  status?: Presence;
+}
+
+
+const mockStudents: Student[] = [
+  { id: '1', firstName: 'Monique', lastName: 'Barrett', status: Presence.Absent },
+  { id: '2', firstName: 'Pauline', lastName: 'Davis', status: Presence.Present },
+  { id: '3', firstName: 'Heidi', lastName: 'Burke' },
+  { id: '4', firstName: 'Jennifer', lastName: 'Townsend' },
+  { id: '5', firstName: 'Casey', lastName: 'McBride' },
+  { id: '6', firstName: 'Diane', lastName: 'Rivera' },
+  { id: '7', firstName: 'Troy', lastName: 'Gutierrez' },
+  { id: '8', firstName: 'Priscilla', lastName: 'Little' },
+  { id: '9', firstName: 'Bobby', lastName: 'Robbins' },
+  { id: '10', firstName: 'Edmund', lastName: 'Gardner' }
+]
 
 const Students: React.FC = () => {
+  const emptyStudent: Student = { id: '', firstName: '', lastName: '' };
+  const [students, setStudents] = useState(mockStudents);
+  const [selectedStudent, setSelectedStudent] = useState(emptyStudent);
+  const [showActionSheet, setShowActionSheet] = useState(false);
+
+  function deleteStudent(student: Student) {
+    setStudents(students.filter(x => x.id !== student.id));
+  }
+
+  async function markAbsent(student: Student) {
+    student.status = Presence.Absent;
+  }
+
+  async function markPresent(student: Student) {
+    student.status = Presence.Present;
+  }
+
+  async function studentDetails(student: Student) {
+    // this.router.navigateByUrl(`/student-info/${student.id}`, {
+    //   state: { student }
+    // })
+  }
+
+  function clickStudent(student: Student) {
+    setSelectedStudent(student);
+    setShowActionSheet(true);
+  }
+
+
   return (
     <IonPage>
       <IonHeader>
@@ -12,18 +73,53 @@ const Students: React.FC = () => {
       </IonHeader>
       <IonContent>
         <IonList>
-          <IonItemSliding>
-            <IonItem>
-              <IonIcon slot="start" icon="person-outline"></IonIcon>
-              <IonLabel>Last Name, First Name</IonLabel>
-              <IonIcon slot="end" icon="eye-outline"></IonIcon>
-              <IonIcon slot="end" icon="eye-off-outline"></IonIcon>
-            </IonItem>
-            <IonItemOptions side="end">
-              <IonItemOption color="danger">Delete</IonItemOption>
-            </IonItemOptions>
-          </IonItemSliding>
+          {students.map((student) => {
+            return (
+              <IonItemSliding key={student.id}>
+                <IonItem onClick={() => clickStudent(student)}>
+                  <IonIcon slot="start" icon={personOutline}></IonIcon>
+                  <IonLabel>{student.lastName}, {student.firstName}</IonLabel>
+                  {student.status === Presence.Present && <IonIcon slot="end" icon={eyeOutline}></IonIcon>}
+                  {student.status === Presence.Absent && <IonIcon slot="end" icon={eyeOffOutline}></IonIcon>}
+                </IonItem>
+                <IonItemOptions side="end">
+                  <IonItemOption color="danger" onClick={() => deleteStudent(student)}>Delete</IonItemOption>
+                </IonItemOptions>
+              </IonItemSliding>
+            );
+          })}
         </IonList>
+        <IonActionSheet isOpen={showActionSheet}
+          header={`${selectedStudent.firstName} ${selectedStudent.lastName}`}
+          onDidDismiss={() => setShowActionSheet(false)}
+          buttons={[{
+            text: 'Delete',
+            role: 'destructive',
+            icon: 'trash',
+            handler: () => { deleteStudent(selectedStudent); }
+          }, {
+            text: 'Mark Present',
+            icon: 'eye-outline',
+            handler: async () => { await markPresent(selectedStudent); }
+          }, {
+            text: 'Mark Absent',
+            icon: 'eye-off-outline',
+            handler: async () => { await markAbsent(selectedStudent); }
+          }, {
+            text: 'Details',
+            icon: 'document-text-outline',
+            handler: () => { studentDetails(selectedStudent); }
+          }, {
+            text: 'Cancel',
+            icon: 'close',
+            role: 'cancel',
+            handler: () => {
+              console.log('Cancel clicked');
+            }
+          }]}
+        >
+
+        </IonActionSheet>
       </IonContent>
     </IonPage>
   );
